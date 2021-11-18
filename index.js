0f9494fcd273327a890de3af144b459dd097deea
xsadia <lipearthur@hotmail.com>
2021-11-18 04:00:31 -0300

    :clown_face:

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+//frequency counter pattern
+function anagram(str1, str2) {
+  if (str1.length !== str2.length) return false;
+
+  let acc = {};
+
+  for (let char of str1) {
+    if (!acc[char]) {
+      acc[char] = 1;
+    } else {
+      acc[char]++;
+    }
+  }
+
+  for (let char of str2) {
+    if (!acc[char]) return false;
+    acc[char]--;
+  }
+
+  return true;
+}
+
+//pointers pattern
 function sumZero(array) {
   let high = array.length - 1;
   let low = 0;
@@ -19,6 +42,8 @@ function sumZero(array) {
 }
 
 function countUniqueValues(array) {
+  if (array.length === 0) return 0;
+
   let left = 0;
   let right = 1;
 
@@ -38,3 +63,5 @@ function countUniqueValues(array) {
 console.log(countUniqueValues([1, 1, 1, 2, 3, 3, 4, 4, 5, 6]));
 //0, 0, 0, 1, 2, 2, 3, 3, 4, 5
 console.log(countUniqueValues([1, 1, 1, 2, 3, 3, 3, 3, 3, 3]));
+console.log(countUniqueValues([1, 1, 2, 2, 3, 3, 4, 5, 6, 6, 7]));
+console.log(countUniqueValues([]));
